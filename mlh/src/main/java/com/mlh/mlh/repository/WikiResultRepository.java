7448f07117728dad4f3d4758a26672d832eb3695
glebka702 <glebmalashkevich@gmail.com>
2025-05-28 18:12:37 +0300

    Delete mlh directory

--- mlh/src/main/java/com/mlh/mlh/repository/WikiResultRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mlh.mlh.repository;
-
-import com.mlh.mlh.model.WikiResult;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WikiResultRepository extends JpaRepository<WikiResult, Long> {
-}
